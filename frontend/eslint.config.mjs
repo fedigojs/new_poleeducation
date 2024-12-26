0bbb2f80d072917217f0aca3fcaf7346baf61ec5
Igor Fedianin <fedigo.sj@gmail.com>
2024-12-26 18:19:43 +0200

    fix bug

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,10 +1,21 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks'; // Импортируем плагин
 
 export default [
-    { files: ['**/*.{js,mjs,cjs,jsx}'] },
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    pluginReact.configs.flat.recommended,
+    {
+        files: ['**/*.{js,mjs,cjs,jsx}'], // Указываем файлы, к которым применяются настройки
+        languageOptions: {
+            globals: globals.browser, // Устанавливаем глобальные переменные
+        },
+        rules: {
+            ...pluginJs.configs.recommended.rules, // Рекомендуемые правила для JS
+            ...pluginReact.configs.flat.recommended.rules, // Рекомендуемые правила для React
+            ...pluginReactHooks.configs.recommended.rules, // Рекомендуемые правила для хуков React
+            // Если нужно, можно добавить кастомные настройки правил хуков:
+            'react-hooks/rules-of-hooks': 'error', // Проверяет правила хуков
+            'react-hooks/exhaustive-deps': 'warn', // Проверяет зависимости хуков useEffect
+        },
+    },
 ];
